2772547547b5acb4d4b53119ca90b0ecce8d8400
jlu7 <tontonrue@gmail.com>
2015-03-07 14:21:29 -0800

    Fixing repo
    Moved a lot of files

--- trunk/MTG Deals/ProjectSettings/Assets/Standard Assets/Scripts/DealFinderScripts/LifeCounter/Calculator/LifeTotalManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public static class LifeTotalManager
-{
-	static List<int> Player1;
-	static List<int> Player2;
-}
