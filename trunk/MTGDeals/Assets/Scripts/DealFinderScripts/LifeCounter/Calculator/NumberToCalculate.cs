10ee12f74856c9a9cc289fd940c56babd229fad6
jlu7 <tontonrue@gmail.com>
2015-03-07 14:23:56 -0800

    apparently more uncommitted changes
    MTG Deals folder now MTGDeals

---
+++ trunk/MTGDeals/Assets/Scripts/DealFinderScripts/LifeCounter/Calculator/NumberToCalculate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberToCalculate : MonoBehaviour {
+
+	public int NumToCalculate = 0;
+
+	public void IncreaseDigit(int num)
+	{
+		NumToCalculate = NumToCalculate * 10 + num;
+	}
+
+	public int Subtract()
+	{
+		return -NumToCalculate;
+	}
+
+	public int Add()
+	{
+		return NumToCalculate;
+	}
+}
