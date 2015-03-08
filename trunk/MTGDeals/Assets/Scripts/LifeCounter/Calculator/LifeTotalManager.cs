33ae0f8dee5f8642463f068f3ec713df7565daae
jlu7 <tontonrue@gmail.com>
2015-03-07 17:04:41 -0800

    Calculator Update
    Working on it

--- trunk/MTGDeals/Assets/Scripts/LifeCounter/Calculator/LifeTotalManager.cs
+++ trunk/MTGDeals/Assets/Scripts/LifeCounter/Calculator/LifeTotalManager.cs
@@ -1,9 +1,34 @@
-﻿using UnityEngine;
+using System.Globalization;
+using System.Linq;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public static class LifeTotalManager
+public class LifeTotalManager : MonoBehaviour
 {
-	static List<int> Player1;
-	static List<int> Player2;
+    private Transform GridRef;
+	public LinkedList<GameObject> LifeTotalHistory;
+    private int History;
+
+    void Start()
+    {
+        GridRef = transform.FindChild("DragablePanel/Grid");
+        LifeTotalHistory = new LinkedList<GameObject>();
+        InstatiateNewNumber(0);
+        InstatiateNewNumber(1);
+        InstatiateNewNumber(2);
+        InstatiateNewNumber(3);
+        InstatiateNewNumber(4);
+        InstatiateNewNumber(5);
+        GridRef.GetComponent<UIGrid>().repositionNow = true;
+    }
+
+    void InstatiateNewNumber(int num)
+    {
+        GameObject newNum = Resources.Load<GameObject>("LifeCounter/Number");
+        newNum.transform.parent = GridRef;
+        newNum.transform.Find("Label").GetComponent<UILabel>().text = num.ToString(CultureInfo.InvariantCulture);
+        newNum.transform.localScale = new Vector3(1, 1, 1);
+        LifeTotalHistory.AddFirst(newNum);
+    }
 }
