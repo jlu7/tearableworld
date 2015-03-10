e202f46193210659ab47438972d5847916286de1
jlu7 <tontonrue@gmail.com>
2015-03-10 01:15:04 -0700

    Life Counter now sorts properly
    title

--- trunk/MTGDeals/Assets/Scripts/LifeCounter/Calculator/LifeTotalManager.cs
+++ trunk/MTGDeals/Assets/Scripts/LifeCounter/Calculator/LifeTotalManager.cs
@@ -7,28 +7,72 @@ using System.Collections.Generic;
 public class LifeTotalManager : MonoBehaviour
 {
     private Transform GridRef;
-	public LinkedList<GameObject> LifeTotalHistory;
-    private int History;
-
+	public LinkedList<LifeTotalObject> LifeTotalHistory;
+	
     void Start()
     {
         GridRef = transform.FindChild("DragablePanel/Grid");
-        LifeTotalHistory = new LinkedList<GameObject>();
-        InstatiateNewNumber(0);
-        InstatiateNewNumber(1);
-        InstatiateNewNumber(2);
-        InstatiateNewNumber(3);
-        InstatiateNewNumber(4);
-        InstatiateNewNumber(5);
-        GridRef.GetComponent<UIGrid>().repositionNow = true;
+        LifeTotalHistory = new LinkedList<LifeTotalObject>();
+		InstantiateNewNumber(13);
+		InstantiateNewNumber(12);
+		InstantiateNewNumber(11);
+		InstantiateNewNumber(10);
+		InstantiateNewNumber(9);
+		InstantiateNewNumber(8);
+		InstantiateNewNumber(7);
+		InstantiateNewNumber(6);
+		InstantiateNewNumber(5);
+		InstantiateNewNumber(4);
+		InstantiateNewNumber(3);
+		InstantiateNewNumber(2);
+		InstantiateNewNumber(1);
+		InstantiateNewNumber(0);
     }
 
-    void InstatiateNewNumber(int num)
+    void InstantiateNewNumber(int num)
     {
-        GameObject newNum = Resources.Load<GameObject>("LifeCounter/Number");
+        GameObject newNum = Instantiate(Resources.Load<GameObject>("LifeCounter/Number")) as GameObject;
+		LifeTotalObject newLTO = new LifeTotalObject(newNum); 
+		newNum.name = "000" + 0;
         newNum.transform.parent = GridRef;
         newNum.transform.Find("Label").GetComponent<UILabel>().text = num.ToString(CultureInfo.InvariantCulture);
         newNum.transform.localScale = new Vector3(1, 1, 1);
-        LifeTotalHistory.AddFirst(newNum);
+		foreach(LifeTotalObject x in LifeTotalHistory)
+		{
+			x.Position++;
+			if (x.Position >= 10)
+			{
+				x.Life.name = "00" + x.Position;
+			}
+			else if (x.Position >= 100)
+			{
+				x.Life.name = "0" + x.Position;
+			}
+			else if (x.Position >= 1000)
+			{
+				x.Life.name = x.Position.ToString();
+			}
+			else if (x.Position >= 10000)
+			{
+				LifeTotalHistory.Remove(x);
+			}
+			else
+			{
+				x.Life.name = "000" + x.Position;
+			}
+		}
+        LifeTotalHistory.AddFirst(newLTO);
+        GridRef.GetComponent<UIGrid>().repositionNow = true;
     }
+}
+
+public class LifeTotalObject
+{
+	public int Position = 0;
+	public GameObject Life;
+
+	public LifeTotalObject(GameObject life)
+	{
+		Life = life;
+	}
 }
